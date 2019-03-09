868eb0be5252173a3ae4af4b01510b6dcaa602b7
Daniel Yadlin <dankayadlin@gmail.com>
2019-03-09 18:56:34 +0200

    Schedule class added

---
+++ src/com/hit/java/models/Schedule.java
@@ -0,0 +1,71 @@
+package com.hit.java.models;
+
+public class Schedule {
+
+    private int activityId;
+    private String name;
+    private int day;
+    private int start;
+    private int end;
+
+    public Schedule(){}
+
+    public Schedule(int activityId, String name, int day, int start, int end) {
+        this.activityId = activityId;
+        this.name = name;
+        this.day = day;
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    public String toString() {
+        return "Schedule{" +
+                "activityId=" + activityId +
+                ", name='" + name + '\'' +
+                ", day=" + day +
+                ", start=" + start +
+                ", end=" + end +
+                '}';
+    }
+
+    public void setActivityId(int activityId) {
+        this.activityId = activityId;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setDay(int day) {
+        this.day = day;
+    }
+
+    public void setStart(int start) {
+        this.start = start;
+    }
+
+    public void setEnd(int end) {
+        this.end = end;
+    }
+
+    public int getActivityId() {
+        return activityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getDay() {
+        return day;
+    }
+
+    public int getStart() {
+        return start;
+    }
+
+    public int getEnd() {
+        return end;
+    }
+}
