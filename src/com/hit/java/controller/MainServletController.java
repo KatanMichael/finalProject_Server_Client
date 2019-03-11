63d753e945e3565a82295305b87226d77f1dcd5f
Shani <shanigeva12@gmail.com>
2019-03-12 00:32:00 +0200

    DONE

--- src/com/hit/java/controller/MainServletController.java
+++ src/com/hit/java/controller/MainServletController.java
@@ -17,6 +17,8 @@ import java.util.List;
 import static java.lang.System.out;
 
 
+
+
 @WebServlet(name = "MainServletController", urlPatterns = {"/controller/*"})
 public class MainServletController extends HttpServlet
 {
